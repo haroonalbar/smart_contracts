a7c3b72d865812c9b1f1687641d41a5ca77ec9f9
haroon <haroonalbarx@gmail.com>
2022-06-08 15:02:55 +0530

    first commit

---
+++ SimpleStorage.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.8;
+
+contract SimpleStorage {
+    //datatypes in solidity
+    uint256 favoriteNumber = 50;
+    string favoriteName = "haroon";
+    bool torf = true;
+    int256 a = -23;
+    address myaddress = 0x2479111c7e372E1E5E83Bd03dD33d30e922e493d;
+    bytes32 data = "cat"; //0x76524234798
+}
