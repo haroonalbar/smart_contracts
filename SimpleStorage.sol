2462c422ee8aa32dfd39e344dc0e157e9379368f
haroon <haroonalbarx@gmail.com>
2022-06-09 12:15:37 +0530

    favoriteNumber

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,11 +2,9 @@
 pragma solidity 0.8.8;
 
 contract SimpleStorage {
-    //datatypes in solidity
-    uint256 favoriteNumber = 50;
-    string favoriteName = "haroon";
-    bool torf = true;
-    int256 a = -23;
-    address myaddress = 0x2479111c7e372E1E5E83Bd03dD33d30e922e493d;
-    bytes32 data = "cat"; //0x76524234798
+    uint256 favoriteNumber;
+
+    function stor(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
 }
